25599028f8b356eb98e1436b10647b16558970a1
Jackie <jjac.lee@gmail.com>
2022-12-22 12:53:03 -0800

    added comments in js file

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ generateBtn.addEventListener("click", writePassword);
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+//variable pLength, possibleChar and object typesChar is global so it can be access throughout the entire code
+
 var pLength = 0;
 var possibleChar = []; 
 var typesChar = {
@@ -40,11 +42,19 @@ function generatePassword() {
   }
   askUser();
   for(i = 0; i < pLength; i++){
-      passwordStorage = passwordStorage + possibleChar[Math.floor(Math.random() * possibleChar.length - 1)];
-      console.log(passwordStorage);
+      passwordStorage = passwordStorage + possibleChar[Math.floor(Math.random() * possibleChar.length)];
   }
   return passwordStorage;
 }
+
+//initialy asked the users for password length, then checks if its more than 8 chars and less than 128 chars. if not then it'll prompt users to 
+//re enter a new number. prompt cycles in a while loop until password length meets requirements. code will know requirement is met when variable 
+//"lengthReq" is turned true on line 67
+
+//after password length met requirements it then cycles through critera questions to check what to include with the password. 
+//if user confirms yes to each criteria. that list of char in that criteria wil be added into an array that stores the list of all possible characters
+//criteria checking is also in a while loop until atleast one criteria is selected. This is tested by a boolean that is stored in the object typesChar
+// while loop conditional is met when atleast one criteria is checked and the variable "selected" is true on line 93
 
 function askUser(){
   pLength = prompt('password length?');
@@ -61,26 +71,20 @@ function askUser(){
         typesChar.upperCase = confirm("Include uppercase?");
         if(typesChar.upperCase) {
           possibleChar.push("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
-          console.log(possibleChar);
         }
-
         typesChar.lowerCase = confirm("Include Lowercase?");
         if(typesChar.lowerCase){
           possibleChar.push("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
-          console.log(possibleChar);
+          
         }
         typesChar.numb = confirm("Include numbers?");
         if(typesChar.numb){
-          possibleChar.push("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
-          console.log(possibleChar);
+          possibleChar.push("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");   
         }
-        
         typesChar.specialChar = confirm("Includes special char?");
         if(typesChar.specialChar){
           possibleChar.push(" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~");
         }
-          console.log(possibleChar);
-
         if(!(typesChar.upperCase || typesChar.lowerCase || typesChar.numb || typesChar.specialChar)){
           alert("you must select atleast one option");
         } else {
